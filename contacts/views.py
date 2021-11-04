978a796aa1817b4f3b3fbdfdd897916513f2fc41
Lila <djyasin.ly@gmail.com>
2021-11-04 13:23:00 -0400

    Got an emoji birthday cake icon rendering to the page.

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Contact
 from .forms import ContactForm
+from .models import Contact, Note
 
 
 # Create your views here.
